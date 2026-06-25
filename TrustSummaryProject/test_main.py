6db3ffc7779c023ee1cf2b2793e771e7c293db79
Stephen Rogers <stephen@gotbean.co.uk>
2026-06-25 11:38:37 +0100

    a

--- TrustSummaryProject/test_main.py
+++ TrustSummaryProject/test_main.py
@@ -1,3 +1,19 @@
 #some basic automated testing
 import unittest
-from main import safe_int
+from main import check_int
+
+
+#test the integer check function
+class TestIntegerCheck(unittest.TestCase):
+    def test_check_int_valid(self):
+        self.assertEqual(check_int("5"), 5)
+    
+    def test_check_int_invalid(self):
+        self.assertEqual(check_int("abc"), 0)
+    
+    def test_check_int_negative(self):
+        self.assertEqual(check_int("-1"), -1)
+
+    def test_check_int_negative(self):
+        self.assertEqual(check_int("5.2"), 5.2)
+
